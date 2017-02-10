9fe657715c13f17f82250fd7664531ffb750e72d
Shabaz Patel <shabaz.b.patel@gmail.com>
2017-02-10 01:16:28 -0800

    updating logger name and confog

--- indexer/logger.py
+++ indexer/logger.py
@@ -4,5 +4,5 @@ sh = logging.StreamHandler()
 fmt = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 sh.setFormatter(fmt)
 
-log = logging.getLogger('analytics-worker')
+log = logging.getLogger('nsfw-analytics-worker')
 log.addHandler(sh)
