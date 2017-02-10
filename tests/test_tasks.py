efe2a302cec2d0502b99bbd9e2cdfdc3ffd058d4
Shabaz Patel <shabaz.b.patel@gmail.com>
2017-02-09 23:08:34 -0800

    hotfix

--- tests/test_tasks.py
+++ tests/test_tasks.py
@@ -4,7 +4,7 @@ import pytest
 import datetime
 from celery.result import AsyncResult
 from indexer.celery_init import celery_app
-from indexer.tasks import analytics_image_worker, analytics_gif_worker
+from indexer.tasks import nsfw_analytics_gif_worker, nsfw_analytics_image_worker
 from indexer.logger import log
 
 TEST_TYPE='celer'
@@ -19,7 +19,7 @@ def test_analytics_nsfw_image_worker():
         task = celery_app.send_task('indexer.tasks.analytics_nsfw_image_worker', args=args)
         result = task.wait()
     else:
-        result = analytics_image_worker(image_url, thumbnail_url)
+        result = nsfw_analytics_gif_worker(image_url, thumbnail_url)
 
     log.info('results' + str(result))
     assert result['status_code'] == 2000
@@ -34,7 +34,7 @@ def test_analytics_nsfw_gif_worker():
         task = celery_app.send_task('indexer.tasks.analytics_nsfw_gif_worker', args=args)
         result = task.wait()
     else:
-        result = analytics_gif_worker(gif_url)
+        result = nsfw_analytics_image_worker(gif_url)
 
     log.info('results' + str(result))
     assert result['status_code'] == 2000
