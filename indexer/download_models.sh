825475b81e2ff99d336e22ddb218d1f9bd606c0f
Shabaz Patel <shabaz.b.patel@gmail.com>
2017-02-22 22:18:12 -0800

    hotfix

--- indexer/download_models.sh
+++ indexer/download_models.sh
@@ -10,5 +10,5 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # Install nsfw models
 if [ ! -f $DIR/nsfw_model/googlenet.caffemodel ]; then
     wget "https://s3-us-west-1.amazonaws.com/acusense-models/cnn/nsfw/googlenet.caffemodel" \
-    -O $DIR/nsfw_model/alexnet.caffemodel
+    -O $DIR/nsfw_model/googlenet.caffemodel
 fi
