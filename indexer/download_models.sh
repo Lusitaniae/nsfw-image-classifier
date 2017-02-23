51f778eb6cf45ff8778df076daab22893f36885e
Shabaz Patel <shabaz.b.patel@gmail.com>
2017-02-22 22:38:49 -0800

    updating model download script

--- indexer/download_models.sh
+++ indexer/download_models.sh
@@ -2,12 +2,6 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Install nsfw models
-# if [ ! -f $DIR/nsfw_model/alexnet.caffemodel ]; then
-#    wget "https://s3-us-west-1.amazonaws.com/acusense-models/cnn/nsfw/alexnet.caffemodel" \
-#    -O $DIR/nsfw_model/alexnet.caffemodel
-# fi
-
-# Install nsfw models
 if [ ! -f $DIR/nsfw_model/googlenet.caffemodel ]; then
     wget "https://s3-us-west-1.amazonaws.com/acusense-models/cnn/nsfw/googlenet.caffemodel" \
     -O $DIR/nsfw_model/googlenet.caffemodel
