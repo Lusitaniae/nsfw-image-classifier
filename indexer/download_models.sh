f05eba8410d7a610e436cf69a2076560ba0480fc
Shabaz Patel <shabaz.b.patel@gmail.com>
2017-02-22 22:13:39 -0800

    hotfix to download model

--- indexer/download_models.sh
+++ indexer/download_models.sh
@@ -2,13 +2,13 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Install nsfw models
-if [ ! -f $DIR/nsfw_model/alexnet.caffemodel ]; then
-    wget "https://s3-us-west-1.amazonaws.com/acusense-models/cnn/nsfw/alexnet.caffemodel" \
-    -O $DIR/nsfw_model/alexnet.caffemodel
-fi
+# if [ ! -f $DIR/nsfw_model/alexnet.caffemodel ]; then
+#    wget "https://s3-us-west-1.amazonaws.com/acusense-models/cnn/nsfw/alexnet.caffemodel" \
+#    -O $DIR/nsfw_model/alexnet.caffemodel
+# fi
 
 # Install nsfw models
-if [ ! -f $DIR/nsfw_model/alexnet.caffemodel ]; then
+if [ ! -f $DIR/nsfw_model/googlenet.caffemodel ]; then
     wget "https://s3-us-west-1.amazonaws.com/acusense-models/cnn/nsfw/googlenet.caffemodel" \
     -O $DIR/nsfw_model/alexnet.caffemodel
 fi
