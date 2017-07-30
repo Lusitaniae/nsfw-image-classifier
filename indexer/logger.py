837463a7218456c92cf3af2329b32b7ad196197d
Anand Sampat <asampat3090@gmail.com>
2017-07-30 12:32:12 -0700

    cleanup, remove celery and any propreitary information

--- indexer/logger.py
+++
@@ -1,8 +0,0 @@
-import logging
-
-sh = logging.StreamHandler()
-fmt = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-sh.setFormatter(fmt)
-
-log = logging.getLogger('nsfw-analytics-worker')
-log.addHandler(sh)
